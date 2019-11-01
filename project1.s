77ff891ed40448f74d5d9241f3850e8f47db81fe
Sabin Gaire <sabin.gaire@bison.howard.edu>
2019-10-31 20:24:34 -0400

    Loops through the string using the loop

--- project1.s
+++ project1.s
@@ -19,3 +19,14 @@
 		li $a1, 11   #This allows the user to only input 10 characters
 		move $t0, $a0	 	#5 in $a1 to store 1000 bytes in buffer
 		syscall
+
+loop_through:									#Loops through all the character of the input string one by one
+
+		lb $a0, ($t0)								#Initializing the address for the character of the string
+
+		add $t0, $t0, 1								#Incrementing the address
+
+		beqz 	$a0, 			end_of_loop			#If "\0" found, end of loop. We know the ascii value of "\0" is zero
+
+		beq  	$a0, 	11, 	end_of_loop			#For string less than 10, the last characters
+																			# is "\n", so checking for that
