52dbef360176e94c044658b41c3d7279e500a5f4
Sabin Gaire <sabin.gaire@bison.howard.edu>
2019-10-31 20:21:10 -0400

    Allows user to input the string they want, the string should contain 10 characters

--- project1.s
+++ project1.s
@@ -14,5 +14,8 @@
 		la $a0, input_message  #This will allow the "Choose a string method to print"
 		syscall
 
-
-	
+		li $v0, 8		#load 8 in $v0 to read string input from user
+		la $a0, Input_user	#load the value given by the users to $a0
+		li $a1, 11   #This allows the user to only input 10 characters
+		move $t0, $a0	 	#5 in $a1 to store 1000 bytes in buffer
+		syscall
