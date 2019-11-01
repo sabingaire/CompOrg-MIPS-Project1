bd682751632b78b1c99d2ba754115f876f203cf1
Sabin Gaire <sabin.gaire@bison.howard.edu>
2019-10-31 20:10:07 -0400

    Added different registes to track different things including the final sum

--- project1.s
+++ project1.s
@@ -2,3 +2,11 @@
 	input_message:     	.asciiz "Choose a string: "
   newline: 			.asciiz "\n"
 	Input_user: .space  10
+
+.text
+		main:
+		la $s1, 0					# s1 is the register reserved to print the result
+		la $s3, 0					# Boolean for checking if a space is found after a valid character
+		la $s4, 0
+
+	
